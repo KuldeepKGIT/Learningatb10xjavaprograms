2387666d72e210114c3a780c41761eaf42e869af
KuldeepKGIT <kuldeepkumar290497@gmail.com>
2025-01-12 20:15:48 +0530

    new prg

---
+++ src/ex_01_Java_Basics/Lab004_tableof9.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basics;
+
+public class Lab004_tableof9 {
+    public static void main(String[] args) {
+
+        System.out.println("Table of Nine");
+        System.out.printf("9 X 1 = %d\n", 9);
+        System.out.printf("9 X 2 = %d\n", 18);
+        System.out.printf("9 X 2 = %d\n", 27);
+        System.out.printf("9 X 2 = %d\n", 36);
+        System.out.printf("9 X 2 = %d\n", 45);
+        System.out.printf("9 X 2 = %d\n", 54);
+        System.out.printf("9 X 2 = %d\n", 63);
+        System.out.printf("9 X 2 = %d\n", 72);
+        System.out.printf("9 X 2 = %d\n", 81);
+        System.out.printf("9 X 2 = %d", 90);
+    }
+}
