3310efce55edfdc6e751860794d703e499ea0a71
KuldeepKGIT <kuldeepkumar290497@gmail.com>
2025-01-24 23:24:28 +0530

    task 19th jan

---
+++ src/learningoperators_02/Lab009_IDtask01.java
@@ -0,0 +1,9 @@
+package learningoperators_02;
+
+public class Lab009_IDtask01 {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(++a + a++ + a++); /* In the first expression value increased by 1 in
+                                                the 2nd expression it was same and in the 3rd expression it again increased by 1*/
+    }
+}
