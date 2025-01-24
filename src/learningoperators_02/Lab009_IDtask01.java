2a8978ad1cce4a50dcc96845e57ef59f96888c58
KuldeepKGIT <kuldeepkumar290497@gmail.com>
2025-01-24 23:28:43 +0530

    task 19th jan

--- src/learningoperators_02/Lab009_IDtask01.java
+++ src/learningoperators_02/Lab009_IDtask01.java
@@ -4,6 +4,6 @@ public class Lab009_IDtask01 {
     public static void main(String[] args) {
         int a=10;
         System.out.println(++a + a++ + a++); /* In the first expression value increased by 1 in
-                                                the 2nd expression it was same and in the 3rd expression it again increased by 1*/
+                                                the 2nd expression it was same and in the 3rd expression value changed by 1, but it is not used*/
     }
 }
