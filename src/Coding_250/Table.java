e8dce7871cb691433181fbb8fea4a13280aa2c78
KuldeepKGIT <kuldeepkumar290497@gmail.com>
2025-03-19 00:09:43 +0530

    Practice

---
+++ src/Coding_250/Table.java
@@ -0,0 +1,18 @@
+package Coding_250;
+
+import java.util.Scanner;
+
+public class Table {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter the input");
+        int n = scanner.nextInt();
+        System.out.println("Table of input number");
+        for(int i= 1; i<=10; i++){
+            int result = n*i;
+//            System.out.printf(n+" X "+i+ " = "+result);
+            System.out.printf("%d X %d = %d\n",n,i,result);
+
+        }
+    }
+}
